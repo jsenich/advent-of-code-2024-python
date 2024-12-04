877020f3a12c202b5728b0fb8e48c629dbfd3326
Jay Senich <1821282+jsenich@users.noreply.github.com>
2024-12-03 22:56:15 -0500

    Day 3 part two

--- day03/day03.py
+++ day03/day03.py
@@ -1,12 +1,28 @@
 import re
 
-
 def part_one(puzzle_input: str) -> int:
     return sum(int(m[0]) * int(m[1]) for m in re.findall(r"mul\((\d+),(\d+)\)", puzzle_input))
+
+def part_two(puzzle_input: str) -> int:
+    enabled: bool = True
+    total: int = 0
+
+    for m in re.findall(r"(mul\((\d+),(\d+)\)|do\(\)|don't\(\))", puzzle_input):
+        if m[0] == "don't()":
+            enabled = False
+            continue
+        elif m[0] == "do()":
+            enabled = True
+        else:
+            if not enabled:
+                continue
+            total += int(m[1]) * int(m[2])
+    
+    return total
 
 if __name__ == "__main__":
     with open('day03/input.txt') as f:
         puzzle_input = f.read().strip()
 
     print(f"part one: {part_one(puzzle_input)}")
-    # print(f"part two: {part_two(puzzle_input)}")
+    print(f"part two: {part_two(puzzle_input)}")
--- tests/day03/test_day03.py
+++ tests/day03/test_day03.py
@@ -1,15 +1,23 @@
 import pytest
-from day03.day03 import part_one
+from day03.day03 import part_one, part_two
 
-sample_puzzle_input = """xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"""
+sample_puzzle_partone = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+sample_puzzle_parttwo = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
 
 with open('day03/input.txt') as f:
     live_puzzle_input = f.read().strip()
  
 
-@pytest.mark.parametrize("puzzle_input,expected", [(sample_puzzle_input, 161), (live_puzzle_input, 189600467)])
+@pytest.mark.parametrize("puzzle_input,expected", [(sample_puzzle_partone, 161), (live_puzzle_input, 189600467)])
 def test_part_one(puzzle_input, expected):
 
     actual = part_one(puzzle_input)
+
+    assert actual == expected
+
+@pytest.mark.parametrize("puzzle_input,expected", [(sample_puzzle_parttwo, 48), (live_puzzle_input, 107069718)])
+def test_part_two(puzzle_input, expected):
+
+    actual = part_two(puzzle_input)
 
     assert actual == expected
