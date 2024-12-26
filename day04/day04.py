0488fcb826ce8ff6faf26c632938398e380b4afb
Jay Senich <1821282+jsenich@users.noreply.github.com>
2024-12-25 23:25:28 -0500

    Day 4 part 2

--- day04/day04.py
+++ day04/day04.py
@@ -1,6 +1,8 @@
 import re
 
 
+type Grid = list[list[str]]
+
 def extract_grid(puzzle_input: str) -> list[list[str]]:
     grid = []
     for line in puzzle_input.splitlines():
@@ -12,7 +14,7 @@ def grid_to_str(grid: list[list[str]]) -> str:
     output = ""
     for line in grid:
         output += f"{''.join(line)}\n"
-    
+
     return output
 
 def rotate_grid(grid: list[list[str]]) -> list[list[str]]:
@@ -73,11 +75,50 @@ def part_one(puzzle_input: str) -> int:
     return xmas_count
 
 def part_two(puzzle_input: str) -> int:
-    pass
+    xmas_count: int = 0
+    grid: Grid = []
+    for line in puzzle_input.splitlines():
+        grid.append(list(line))
+
+    row_max: int = len(grid)
+    col_max: int = len(grid[0])
+
+    for row, _ in enumerate(grid):
+        if row + 2 >= row_max:
+            break
+        for col, _ in enumerate(grid):
+            if col + 2 >= col_max:
+                break
+
+            if (
+                grid[row][col] in ("M", "S") and
+                grid[row][col+2] in ("M", "S") and
+                grid[row+1][col+1] == "A" and
+                grid[row+2][col] in ("M", "S") and
+                grid[row+2][col+2] in ("M", "S") and
+                grid[row][col] != grid[row+2][col+2] and
+                grid[row][col+2] != grid[row+2][col]
+            ):
+                xmas_count += 1
+
+    return xmas_count
 
 if __name__ == "__main__":
     with open('day04/input.txt') as f:
         puzzle_input = f.read().strip()
 
-    print(f"part one: {part_one(puzzle_input)}")
-    # print(f"part two: {part_two(puzzle_input)}")
+    print(f"part one: {part_one(puzzle_input)}") # Answer: 2434
+
+#     puzzle_input = """.M.S......
+# ..A..MSMS.
+# .M.S.MAA..
+# ..A.ASMSM.
+# .M.S.M....
+# ..........
+# S.S.S.S.S.
+# .A.A.A.A..
+# M.M.M.M.M.
+# ..........
+# """.strip()
+
+    print(f"part two: {part_two(puzzle_input)}") # Answer: 1835
